c4cf16793d534b2f1b8ac10035d227796ecf3ed2
iamrahatkabir <iftyrahatkabir@gmail.com>
2025-08-20 06:32:20 +0600

    Path printing of path from source to destination in shortes distance

--- 7_single_source_shortest_distance_path_printing.cpp
+++ 7_single_source_shortest_distance_path_printing.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 vector<int> adj_list[1005];
@@ -14,32 +14,42 @@ void bfs(int src)
     visited_arr[src] = true;
     level[src] = 0;
 
-    while(!q.empty())
+    while (!q.empty())
     {
         int par = q.front();
         q.pop();
 
-        for(auto child : adj_list[par])
+        for (auto child : adj_list[par])
         {
-            if(!visited_arr[child])
+            if (!visited_arr[child])
             {
                 q.push(child);
                 visited_arr[child] = true;
-                level[child] = level[par]+1;
+                level[child] = level[par] + 1;
                 parent[child] = par;
             }
         }
+
+        // for (int i = 0; i < adj_list[par].size(); i++)
+        // {
+        //     int child = adj_list[par][i]; // manually access element
+        //     if (!visited_arr[child])
+        //     {
+        //         q.push(child);
+        //         visited_arr[child] = true;
+        //         level[child] = level[par] + 1;
+        //         parent[child] = par;
+        //     }
+        // }
     }
-
 }
-
 
 int main()
 {
     int n, e;
     cin >> n >> e;
 
-    while(e--)
+    while (e--)
     {
         int a, b;
         cin >> a >> b;
@@ -57,18 +67,17 @@ int main()
 
     bfs(src);
 
-
     vector<int> path;
 
     int node = destination;
-    while(node != -1)
+    while (node != -1)
     {
         path.push_back(node);
         node = parent[node];
     }
 
     reverse(path.begin(), path.end());
-    for(int x : path)
+    for (int x : path)
     {
         cout << x << " ";
     }
